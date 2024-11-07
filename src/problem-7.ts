bb9734dacb2b4d78d98ab4157b3c59af173f4e47
Nazmul Hassan <nazmulnhb007@yahoo.com>
2024-11-07 12:45:21 +0600

    solved problem #7

---
+++ src/problem-7.ts
@@ -0,0 +1,15 @@
+// define class
+class Car {
+	constructor(
+		public make: string,
+		public model: string,
+		public year: number,
+	) {}
+
+	getCarAge(): number {
+		return new Date().getFullYear() - this.year;
+	}
+}
+
+// const car = new Car('Honda', 'Civic', 2018);
+// console.log(car.getCarAge());
