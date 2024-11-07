5c89e8f0ca5dbb416a0a0949ce69a97f71dba94d
Nazmul Hassan <nazmulnhb007@yahoo.com>
2024-11-07 12:30:47 +0600

    solved problem #5

---
+++ src/problem-5.ts
@@ -0,0 +1,16 @@
+// define function type
+type GetProperty = <T, K extends keyof T>(obj: T, key: K) => T[K];
+
+/**
+ * 
+ * @param obj - An object with any shape
+ * @param key - A key that exists in the provided object
+ * @returns Returns the property value of the matching key
+ */
+const getProperty: GetProperty = (obj, key) => {
+	return obj[key];
+};
+
+// const person = { name: 'Alice', age: 30 };
+// console.log(getProperty(person, 'name'));
+// console.log(getProperty(person, 'hakuna'));
