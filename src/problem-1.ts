7db14d937eddddf6cca5d14814abdc53e9f834ed
Nazmul Hassan <nazmulnhb007@yahoo.com>
2024-11-07 10:47:18 +0600

    solved problem #1

---
+++ src/problem-1.ts
@@ -0,0 +1,13 @@
+// declare function type
+type SumArray = (numbers: number[]) => number;
+
+/**
+ *
+ * @param numbers - Accepts an array of numbers only.
+ * @returns Returns the sum of the numbers of the provided array.
+ */
+const sumArray: SumArray = (numbers) => {
+	return numbers.reduce((acc, number) => acc + number, 0);
+};
+
+console.log(sumArray([1, 2, 3, 4, 5]));
