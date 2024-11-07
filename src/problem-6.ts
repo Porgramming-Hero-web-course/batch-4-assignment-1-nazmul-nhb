37dee5a789102d33c35679cc456b62aa572dfaad
Nazmul Hassan <nazmulnhb007@yahoo.com>
2024-11-07 12:39:23 +0600

    solved problem #6

---
+++ src/problem-6.ts
@@ -0,0 +1,25 @@
+// define interface
+interface Profile {
+	name: string;
+	age: number;
+	email: string;
+}
+
+// define function type
+type UpdateProfile = (
+	profile: Profile,
+	updateOptions: Partial<Profile>,
+) => Profile;
+
+/**
+ *
+ * @param profile - An object of type Profile
+ * @param updateOptions - An object of type Partial of Profile representing the updates
+ * @returns Returns the updated profile
+ */
+const updateProfile: UpdateProfile = (profile, updateOptions) => {
+	return { ...profile, ...updateOptions };
+};
+
+// const myProfile = { name: 'Alice', age: 25, email: 'alice@example.com' };
+// console.log(updateProfile(myProfile, { age: 26, name: 'Abdul' }));
