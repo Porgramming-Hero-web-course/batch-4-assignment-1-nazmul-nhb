c78687320985fd25b5115fe0033673095caf9419
Nazmul Hassan <nazmulnhb007@yahoo.com>
2024-11-07 11:31:38 +0600

    solved problem #2

--- src/problem-2.ts
+++ src/problem-2.ts
@@ -1,2 +1,23 @@
-{
-}
+// define function type
+type RemoveDuplicates = (numbers: number[]) => number[];
+
+/**
+ *
+ * @param numbers - Accepts an array of numbers only.
+ * @returns Returns a new array with duplicates removed.
+ */
+const removeDuplicates: RemoveDuplicates = (numbers) => {
+	// Could have done it in 1 line using Set
+
+	const cleanArr: number[] = [];
+
+	for (const number of numbers) {
+		if (!cleanArr.includes(number)) {
+			cleanArr.push(number);
+		}
+	}
+
+	return cleanArr;
+};
+
+// console.log(removeDuplicates([1, 2, 2, 3, 4, 4, 5]));
