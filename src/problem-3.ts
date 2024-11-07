1189072d61b00a86e6a1cf6ead314aec888c5479
Nazmul Hassan <nazmulnhb007@yahoo.com>
2024-11-07 18:12:33 +0600

    made syntax of solution to proble #3 simpler

--- src/problem-3.ts
+++ src/problem-3.ts
@@ -2,8 +2,5 @@
 type CountWordOccurrences = (sentence: string, keyword: string) => number;
 
 const countWordOccurrences: CountWordOccurrences = (sentence, keyword) => {
-	const wordCount: number =
-		sentence.toLowerCase().split(keyword.toLowerCase()).length - 1;
-
-	return wordCount;
+	return sentence.toLowerCase().split(keyword.toLowerCase()).length - 1;
 };
